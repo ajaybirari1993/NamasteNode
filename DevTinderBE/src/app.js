4d98b96f0e86e61d281dee9c4c9ce3668687e4de
Ajay Birari <ajaybirari@Ajays-MacBook-Pro.local>
2026-05-06 16:46:00 +0530

    Routing added for user

--- DevTinderBE/src/app.js
+++ DevTinderBE/src/app.js
@@ -7,8 +7,32 @@ app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.get("/home", (req, res) => {
+app.get("/ab*cd", (req, res) => {
   res.send("Welcome to the DevTinder API!");
+});
+
+app.get("/user", (req, res) => {
+  console.log("Query parameters:", req.query);
+  const user = {
+    id: 1,
+    name: "Ajay Birari",
+    profession: "Software Developer",
+  };
+  res.json(user);
+});
+
+app.post("/user", (req, res) => {
+  res.send("User created successfully!");
+});
+
+app.delete("/user/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(`User with id ${userId} deleted successfully!`);
+});
+
+app.patch("/user/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(`User with id ${userId} updated successfully!`);
 });
 
 app.listen(PORT, () => {
