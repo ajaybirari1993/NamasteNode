31fc7fff4f6151a7e0ce730c4e9b2772e998b0ea
Ajay Birari <ajaybirari1993@gmail.com>
2026-06-30 22:58:49 +0530

    User feed api added

--- DevTinderBE/src/routes/user.js
+++ DevTinderBE/src/routes/user.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userAuth } from "../middlewares/auth.js";
 import ConnectionRequestModel from "../models/connectionRequest.js";
+import UserModel from "../models/user.js";
 
 const userRouter = express.Router();
 const USER_DATA = ["firstName", "lastName", "gender"];
@@ -9,12 +10,12 @@ userRouter.get("/request/receive", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
-    console.log(loggedInUser._id);
-
     const connectionRequests = await ConnectionRequestModel.find({
       toUserId: loggedInUser._id,
       status: "interested",
     }).populate("fromUserId", USER_DATA);
+
+    console.log(connectionRequests);
 
     res.status(200).json({
       message: "Data fetched successfully",
@@ -51,6 +52,40 @@ userRouter.get("/connections", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).send("ERROR: " + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skipRecords = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    // .populate("fromUserId", ["firstName"])
+    // .populate("toUserId", ["firstName"]);
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const usersToShow = await UserModel.find({
+      _id: { $nin: [...hideUsersFromFeed, loggedInUser._id] },
+    })
+      .select("firstName lastName gender bio")
+      .skip(skipRecords)
+      .limit(limit);
+
+    res.send(usersToShow);
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
   }
 });
 
